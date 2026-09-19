e7bc7df0d3b78d21d557476878175adc77aba1e4
Hyunsung Ha <ae20130@gmail.com>
2026-09-19 16:33:15 +0900

    feat: Device 인터페이스 추가
    - IValve, IPump, IAnologSensor 추가

---
+++ src/Hhs.Automation.Core/IAnalogSenosor.cs
@@ -0,0 +1,11 @@
+﻿namespace Hhs.Automation.Core
+{
+    public interface IAnalogSensor
+    {
+        string Name { get; }                // 명칭
+        bool Error { get; }                 // 에러 발생 여부
+        uint ErrorID { get; }                // 발생한 에러 ID - 구현 시 Enum으로 선언
+        double RawValue { get; }              // Raw 값
+        double Value { get; }                 // 가공된 결과 값
+    }
+}
---
+++ src/Hhs.Automation.Core/IPump.cs
@@ -0,0 +1,22 @@
+﻿namespace Hhs.Automation.Core
+{
+    public enum PumpState
+    {
+        Unknown = 0,
+        Running = 1,
+        Stopped = 2
+    }
+
+    public interface IPump
+    {
+        string Name { get; }                // 명칭
+        bool Error { get; }                 // 에러 발생 여부
+        int ErrorID { get; }                // 발생한 에러 ID - 구현 시 Enum으로 선언
+        bool RunInterlock { get; }          // Run 동작 전 확인하는 Interlock 신호
+        bool StopInterlock { get; }         // Stop 동작 전 확인하는 Interlock 신호
+        PumpState State { get; }            // 동작 상태
+
+        Task<bool> RunAsync(CancellationToken ct);      // Run 명령 - 반환값으로 수행 가능 여부 반환
+        Task<bool> StopAsync(CancellationToken ct);     // Stop 명령 - 반환값으로 수행 가능 여부 반환
+    }
+}
